05c4170e30ca7fc310d187f73ac8ef17531e399d
Arkanium77 <arkanium77@gmail.com>
2025-06-04 11:26:57 +0300

    Measure aspect

---
+++ src/main/java/org/example/pivo/aop/MeasureAspect.java
@@ -0,0 +1,59 @@
+package org.example.pivo.aop;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class MeasureAspect {
+
+    private final ObjectMapper objectMapper;
+
+    @Pointcut("within(org.example.pivo.controller..*)")
+    public void anyControllerMethod() {
+    }
+
+    @Pointcut("@annotation(org.example.pivo.annotation.Measure)")
+    public void annotatedWithMeasure() {
+    }
+
+    @Around("anyControllerMethod() || annotatedWithMeasure()")
+    public Object logExecutionTime(ProceedingJoinPoint pjp) throws Throwable {
+        var method = ((MethodSignature) pjp.getSignature()).getMethod();
+        var className = pjp.getTarget().getClass().getSimpleName();
+        var methodName = method.getName();
+
+        String argsJson;
+        try {
+            argsJson = objectMapper.writeValueAsString(pjp.getArgs());
+        } catch (Exception e) {
+            argsJson = "[unserializable args]";
+        }
+
+        var start = System.currentTimeMillis();
+        try {
+            var result = pjp.proceed();
+            var duration = System.currentTimeMillis() - start;
+            log.info("@Measure: \n✓ {}#{} completed in {}ms with args: {}", className, methodName, duration, argsJson);
+            return result;
+        } catch (Throwable ex) {
+            var duration = System.currentTimeMillis() - start;
+            log.error("@Measure: \n✗ {}#{} failed in {}ms with args: {}",
+                    className,
+                    methodName,
+                    duration,
+                    argsJson,
+                    ex);
+            throw ex;
+        }
+    }
+}
--- src/main/java/org/example/pivo/service/BeerService.java
+++ src/main/java/org/example/pivo/service/BeerService.java
@@ -1,6 +1,7 @@
 package org.example.pivo.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.pivo.annotation.Measure;
 import org.example.pivo.mapper.BeerMapper;
 import org.example.pivo.model.dto.BeerDto;
 import org.example.pivo.model.dto.CreateBeerDto;
@@ -17,7 +18,6 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,10 +27,11 @@ public class BeerService {
     private final TypeRepository typeRepository;
     private final BeerMapper beerMapper;
 
+    @Measure
     public BeerDto create(CreateBeerDto beer) {
         BeerEntity beerEntity = beerMapper.toEntity(beer);
         var typeEntity = typeRepository.findByName(beer.getTypeName())
-                .orElseThrow(()-> new NotFoundPivoException("Тип не найден"));
+                .orElseThrow(() -> new NotFoundPivoException("Тип не найден"));
         beerEntity.setType(typeEntity.getId());
         beerEntity = beerRepository.save(beerEntity);
         return beerMapper.toDto(beerEntity, typeEntity);
@@ -39,7 +40,7 @@ public class BeerService {
     public Page<BeerDto> getAll(Integer pageNumber, Integer pageSize) {
         var result = new ArrayList<BeerDto>();
         var beers = beerRepository.findAll(PageRequest.of(pageNumber, pageSize));
-        if(beers == null){
+        if (beers == null) {
             return Page.empty();
         }
         var typeIds = beers.stream()
@@ -54,7 +55,7 @@ public class BeerService {
 
     public BeerDto get(String id) {
         var beerEntity = beerRepository.findById(id)
-                .orElseThrow(()-> new NotFoundPivoException("Предоставлен неверный id"));
+                .orElseThrow(() -> new NotFoundPivoException("Предоставлен неверный id"));
         var type = typeRepository.findById(beerEntity.getType())
                 .orElseThrow(() -> new NotFoundPivoException("Тип не найден"));
         return beerMapper.toDto(beerEntity, type);
